8894fbb8f09726b88bbb0a88e954989624771b6a
automatic-meow-machine <85621427+automatic-meow-machine@users.noreply.github.com>
2022-05-28 22:36:18 +0800

    feat: 修改对log操作代码的位置

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,12 @@
 # 加载
+import os
+
+if not os.path.exists("log"):
+    os.mkdir("log")
+
+os.environ["QQBOT_LOG_PATH"] = os.path.join(os.getcwd(), "log", "%(name)s.log")
+os.environ[
+    "QQBOT_LOG_PRINT_FORMAT"] = "%(asctime)s \033[1;33m[%(levelname)s] %(funcName)s (%(filename)s:%(lineno)s):\033[0m %(message)s"
 
 
 from core.bot import bot
